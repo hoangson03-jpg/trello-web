011353feb82a1ae2f5d8963e3a837de519eba2b5
hoangson03-jpg <billtommy0@gmail.com>
2024-08-09 12:29:00 +0700

    LAST UPDATE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-  apiRoot = 'https://trello-api-k783.onrender.com/'
+  apiRoot = 'https://trello-api-k783.onrender.com'
 }
 export const API_ROOT = apiRoot
 
